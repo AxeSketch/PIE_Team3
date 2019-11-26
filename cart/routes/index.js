114d0efa6dc6ff43535e3eb24a7a0ef53fd6cbae
Alexandru Buruiana <axesketch@gmail.com>
2019-11-27 00:06:18 +0200

    Shoes cards

--- cart/routes/index.js
+++ cart/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Cos de cumparaturi' });
+  res.render('shop/index', { title: 'Cos de cumparaturi' });
 });
 
 module.exports = router;
