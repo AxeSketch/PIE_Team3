fc34253198cd39f2e6f09d101c2a1d709c82b83d
Alexandru Buruiana <axesketch@gmail.com>
2019-11-26 23:49:07 +0200

    Bara de navigare

--- cart/routes/index.js
+++ cart/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Cos de cumparaturi' });
 });
 
 module.exports = router;
