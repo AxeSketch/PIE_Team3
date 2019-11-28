61216aa28ecef027994b0f916e3feb83a89a35f2
Alexandru Buruiana <axesketch@gmail.com>
2019-11-28 15:54:24 +0200

    Package

--- cart/seed/product-seeder.js
+++ cart/seed/product-seeder.js
@@ -8,19 +8,27 @@ var products = [
     new Product({
     imaginePath:'https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
     titlu: 'Pantof1',
-    descriere:'Descriere!!!',
+    descriere:'Descrierea pantofului 1',
     pret: 90
 }),
 
 new Product({
     imaginePath:'https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
     titlu: 'Pantof2',
-    descriere:'Descriere!!!',
+    descriere:'Descrierea pantofului 2',
+    pret: 90
+}),
+
+new Product({
+    imaginePath:'https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
+    titlu: 'Pantof3',
+    descriere:'Descrierea pantofului 3.',
     pret: 90
 })
+
 ];
 
-var done=0;
+var done=0; 
 
 for (var i=0; i<products.length;i++)
 {
